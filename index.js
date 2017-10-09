4e556a8b1f0f76790785d2db2df22b2fcd419c43
Molunerfinn <marksz@teamsz.xyz>
2017-10-09 18:16:36 +0800

    Added: eslint & commander

--- index.js
+++ index.js
@@ -1,83 +1,98 @@
-var cheerio = require('cheerio');  
-var request = require('sync-request');  
-var fs = require('fs');  
-var http = require('http');
+const cheerio = require('cheerio')
+const axios = require('axios')
+const fs = require('fs')
+const program = require('commander')
+const pjson = require('./package.json')
 
-var url = 'http://movie.douban.com/subject/25724855/'
-// var url = 'http://movie.douban.com/subject/21937445/?source=new_aladdin'
+const url = 'http://movie.douban.com/subject/25724855/'
 
-var html = '';
-var IMDBLink = '';
+let IMDBLink = ''
+let file = 'dbmovie.md'
+let writeFlag = false
 
-html = request('GET', url).getBody().toString();
+program
+  .version(pjson.version)
+  .allowUnknownOption()
+  .option('-w, --write [file]', 'write')
+  .parse(process.argv)
 
-handleDB(html);
+console.log(program.write)
 
-Link = request('GET', IMDBLink).getBody().toString();
+const main = async () => {
+  const html = await axios.get(url)
+  handleDB(html.data)
+  const IMDBData = await axios.get(IMDBLink)
+  handleIMDB(IMDBData.data)
+}
 
-handleIMDB(Link);
+main()
 
 // 获取豆瓣电影里的大部分所需内容
-function handleDB(html){
-  var $ = cheerio.load(html);
-  var info = $('#info');
+function handleDB (html) {
+  let $ = cheerio.load(html)
   // 获取电影名
-  var movieName = $('#content>h1>span').filter(function(i,el){
-    return $(this).attr('property') === 'v:itemreviewed';
-  }).text();
+  let movieName = $('#content>h1>span').filter(function (i, el) {
+    return $(this).attr('property') === 'v:itemreviewed'
+  }).text()
   // 获取影片导演名
-  var directories = '- 导演：' + $('#info span a').filter(function(i,el){
-    return $(this).attr('rel') === 'v:directedBy';
-  }).text();
+  let directories = '- 导演：' + $('#info span a').filter(function (i, el) {
+    return $(this).attr('rel') === 'v:directedBy'
+  }).text()
   // 获取影片演员
-  var starsName = '- 主演：';
-  $('.actor .attrs a').each(function(i,elem){
-      starsName += $(this).text() + '/';
-  });
+  let starsName = '- 主演：'
+  $('.actor .attrs a').each(function (i, elem) {
+    starsName += $(this).text() + '/'
+  })
+
+  starsName = starsName.substring(0, starsName.length)
   // 获取片长
-  var runTime = '- 片长：' + $('#info span').filter(function(i,el){
-    return $(this).attr('property') === 'v:runtime';
-  }).text();
+  var runTime = '- 片长：' + $('#info span').filter(function (i, el) {
+    return $(this).attr('property') === 'v:runtime'
+  }).text()
   // 获取影片类型  
-  var kind = $('#info span').filter(function(i,el){
+  var kind = $('#info span').filter(function (i, el) {
     return $(this).attr('property') === 'v:genre'
-  }).text();
-    // 处理影片类型数据
-  var kLength = kind.length;
-  var kinds = '- 影  片类型：';
-  for (i = 0; i < kLength; i += 2){
-    kinds += kind.slice(i,i+2) + '/';
+  }).text()
+  // 处理影片类型数据
+  var kLength = kind.length
+  var kinds = '- 影  片类型：'
+  for (let i = 0; i < kLength; i += 2) {
+    kinds += kind.slice(i, i + 2) + '/'
   }
+
+  kinds = kinds.substring(0, kinds.length - 1)
   // 获取电影评分和电影评分人数
-    // 豆瓣
-  var DBScore = $('.ll.rating_num').text();
-  var DBVotes = $('a.rating_people>span').text().replace(/\B(?=(\d{3})+$)/g,',');
-  var DB = '- 豆  瓣评分：' + DBScore + '/10' + '(' + 'from' + DBVotes + 'users' + ')';
-    // IMDBLink
-  IMDBLink = $('#info').children().last().prev().attr('href');
+  // 豆瓣
+  var DBScore = $('.ll.rating_num').text()
+  var DBVotes = $('a.rating_people>span').text().replace(/\B(?=(\d{3})+$)/g, ',')
+  var DB = '- 豆  瓣评分：' + DBScore + '/10' + '(' + 'from' + DBVotes + 'users' + ')'
+  // IMDBLink
+  IMDBLink = $('#info').children().last().prev().attr('href')
 
-  var data = movieName + '\r\n' + directories + '\r\n' + starsName + '\r\n' + runTime + '\r\n' + kinds + '\r\n'+ DB +'\r\n';
+  var data = movieName + '\r\n' + directories + '\r\n' + starsName + '\r\n' + runTime + '\r\n' + kinds + '\r\n' + DB + '\r\n'
 
-  fs.appendFile('dbmovie.txt', data, 'utf-8', function(err){
-    if (err) throw err;
-    else console.log('大体信息写入成功'+'\r\n' + data)
-  });
-
+  if (writeFlag) {
+    fs.appendFile(file, data, 'utf-8', function (err) {
+      if (err) throw err
+      else console.log('大体信息写入成功' + '\r\n' + data)
+    })
+  }
 }
 
 // 获取IMDB相关评分和评分人数
-function handleIMDB(Link){
-  var $ = cheerio.load(Link);
-  var IMDBScore = $('.ratingValue span').filter(function(i,el){
-    return $(this).attr('itemprop') === 'ratingValue';
-  }).text();
-  var IMDBVotes = $('.small').filter(function(i,el){
-    return $(this).attr('itemprop') === 'ratingCount';
-  }).text();
-  var IMDB = '- IMDB评分：' + IMDBScore + '/10' + '(' + 'from' + IMDBVotes + 'users' + ')' + '\r\n';
-  fs.appendFile('dbmovie.txt', IMDB, 'utf-8', function(err){
-    if (err) throw err;
-    else console.log('IMDB信息写入成功' + '\r\n' + IMDB)
-  });
+function handleIMDB (Link) {
+  var $ = cheerio.load(Link)
+  var IMDBScore = $('.ratingValue span').filter(function (i, el) {
+    return $(this).attr('itemprop') === 'ratingValue'
+  }).text()
+  var IMDBVotes = $('.small').filter(function (i, el) {
+    return $(this).attr('itemprop') === 'ratingCount'
+  }).text()
+  var IMDB = '- IMDB评分：' + IMDBScore + '/10' + '(' + 'from' + IMDBVotes + 'users' + ')' + '\r\n'
+  if (writeFlag) {
+    fs.appendFile(file, IMDB, 'utf-8', function (err) {
+      if (err) throw err
+      else console.log('IMDB信息写入成功' + '\r\n' + IMDB)
+    })
+  }
 }
-
