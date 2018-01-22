52a4d0df2ddc2d440389a64af407f5ef23b6859d
Molunerfinn <marksz@teamsz.xyz>
2018-01-22 16:28:56 +0800

    Fixed: get IMDB info bugs

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function handleDB (html) {
   var DBVotes = $('a.rating_people>span').text().replace(/\B(?=(\d{3})+$)/g, ',')
   var DB = '- 豆  瓣评分：' + DBScore + '/10' + '(' + 'from' + DBVotes + 'users' + ')'
   // IMDBLink
-  IMDBLink = $('#info').children().last().prev().attr('href')
+  IMDBLink = $('#info').find('[rel="nofollow"]').attr('href')
 
   var data = movieName + '\r\n' + directories + '\r\n' + starsName + '\r\n' + runTime + '\r\n' + kinds + '\r\n' + DB + '\r\n'
 
